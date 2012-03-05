a55b716106f8ecc5807b587732f6d4eeaee26cf7
Lucas De Marchi <lucas.demarchi@profusion.mobi>
2012-03-05 19:39:27 -0300

    Use a BT_Init.sh script per platform as in released version

--- scripts/BT_Init_am335x.sh
+++ scripts/BT_Init_am335x.sh
@@ -7,10 +7,10 @@ rm   /var/run/dbus/pid &> /dev/null
 sleep 1
 dbus-daemon --system &> /dev/null &
 sleep 2
-bluetoothd -n &> /dev/null &
+#bluetoothd -n &> /dev/null &
 insmod `find /lib/modules/ -name "gpio_en.ko"`
 sleep 2
-hciattach /dev/ttyS1 texas 2000000 &
+hciattach /dev/ttyO1 texas 3000000 &
 sleep 10
 hciconfig hci0 piscan &> /dev/null
 agent --path /org/bluez/agent 0000 &> /dev/null &
